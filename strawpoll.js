68179abddbb35f51476732f21a16beadc7733b87
Adam Tutor <atutor@basecampcodingacademy.org>
2016-11-10 16:16:14 -0600

    added questions as JSON.

---
+++ strawpoll.js
@@ -0,0 +1,6 @@
+var Polls = [
+    { Q1:{
+        Question: "Pepsi or Coke?", choice1: "Pepsi", choice2: "Coke"} },
+    { Q2: "Donald or Hillary?", choice1: "Donald", choice2: "Hillary" },
+    { Q3: "Ice Cream or Frozen Yogurt?", choice1: "Ice Cream", choice2: "Frozen Yogurt" }
+];
