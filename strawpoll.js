ec8bf6d606ece91b6d5eb92efb394c9fe4d47fd3
Adam Tutor <atutor@basecampcodingacademy.org>
2016-11-11 13:03:17 -0600

    Made changes to JSON file

--- strawpoll.js
+++ strawpoll.js
@@ -1,6 +1,19 @@
-var Polls = [
-    { Q1:{
-        Question: "Pepsi or Coke?", choice1: "Pepsi", choice2: "Coke"} },
-    { Q2: "Donald or Hillary?", choice1: "Donald", choice2: "Hillary" },
-    { Q3: "Ice Cream or Frozen Yogurt?", choice1: "Ice Cream", choice2: "Frozen Yogurt" }
-];
+var polls = [{
+    "question": "Pepsi or Coke?",
+    "choices": [{
+        "name": "Pepsi",
+        "count": 1
+    }, {
+        "name": "Coke",
+        "count": 2
+    }]
+}, {
+    "question": "Mtn Dew or Mello Yello?",
+    "choices": [{
+        "name": "Mtn Dew",
+        "count": 1
+    }, {
+        "name": "Mello Yello",
+        "count": 2
+    }]
+}];
