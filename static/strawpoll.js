bea4859fed0d7458dab72c3572f156b15c71dfab
Adam Tutor <atutor@basecampcodingacademy.org>
2016-11-16 16:12:36 -0600

    fixed bug, added wrapping on next button and hard stop on previous.

--- static/strawpoll.js
+++ static/strawpoll.js
@@ -1,14 +1,39 @@
 
 var strawpolls;
+var num = 0;
+var box = $(".boxed");
 
+
+function draw() {
+  box.empty();
+  box.append("<p>POLL</p>")
+  box.append("<p>"+ strawpolls[num].question+"</p>");
+  box.append("<input type='radio' name='choice' value ='pepsi'>"+  strawpolls[num].choices[0].label+ "<br>")
+  box.append("<input type='radio' name='choice' value ='coke'>"+  strawpolls[num].choices[1].label)
+
+}
 
 $(document).ready(function() {
+    $("#b3").click(function(){
+      num = (num + 1) % strawpolls.length;
+      draw();
+    });
+    $("#b1").click(function(){
+     num -= 1;
+     if(num < 0){num = 0}
+
+      draw();
+    });
+
     $.getJSON('strawpolls').done(function(json) {
         strawpolls = json.data;
         console.log(json);
-        $(".boxed").append("<p>"+ strawpolls[0].question+"</p>");
-        $(".boxed").append("<input type='radio' name='choice' value ='pepsi'>"+  strawpolls[0].choices[0].label+ "<br>")
-        $(".boxed").append("<input type='radio' name='choice' value ='coke'>"+  strawpolls[0].choices[1].label)
-        $("bi").append()
+        draw();
     });
 });
+
+
+
+
+
+// $("#b1").replaceWith("<button id='b1' type='button'>PREVIOUS</button>")
