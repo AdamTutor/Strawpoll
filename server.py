d248345d4ecb9de41ddd7e95f54bcb56d54452ee
Jacob Spence <jspence@basecampcodingacademy.org>
2016-11-18 09:12:47 -0600

    added create page and made more of the look actually look good

--- server.py
+++ server.py
@@ -14,12 +14,21 @@ def votes(id):
 def root():
     return open("static/index.html").read()
 
+
 @app.route("/strawpolls/<int:id>/vote/<choice>", methods=["POST"])
 def vote(id, choice):
     polls = json.load(open("static/strawpolls.json"))
     polls['data'][id]['choices'][choice] += 1
     json.dump(polls, open("static/strawpolls.json", "w"))
     return jsonify(polls['data'][id])
+
+
+@app.route("/<page>")
+def other(page):
+    print(page)
+    content = open("static/{}.html".format(page)).read()
+    print(content)
+    return content
 
 
 if __name__ == "__main__":
