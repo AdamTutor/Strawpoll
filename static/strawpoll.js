a0651a35470d27dde7bbf4bdf8daaa6e4d2c5026
Adam Tutor <atutor@basecampcodingacademy.org>
2016-11-18 14:35:00 -0600

    Submit button now updates JSON file.

--- static/strawpoll.js
+++ static/strawpoll.js
@@ -1,43 +1,47 @@
-
 var strawpolls;
 var num = 0;
 var box = $(".boxed");
 
-function vote(){
-  "..."
-}
-
-
 function draw() {
-  box.empty();
-  box.append("<p>POLL</p>")
-  box.append("<p>"+ strawpolls[num].question+"</p>");
-  Object.keys(strawpolls[num].choices).forEach(function(c) {
-    box.append("<input type='radio' name='choice' value='" + c + "'>"+ c + "<br>")
-  });
+    box.empty();
+    box.append("<p>POLL</p>")
+    box.append("<p>" + strawpolls[num].question + "</p>");
+    Object.keys(strawpolls[num].choices).forEach(function(c) {
+        box.append("<input type='radio' name='choice' value='" + c + "'>" + c + "<br>")
+    });
 };
 
-function drawResults(){
-  box.empty();
-  Object.keys(strawpolls[num].choices).forEach(function(c) {
-    box.append("<p>" + c + ": "+ strawpolls[num].choices[c] + "</p>")
-  });
+function drawResults() {
+    box.empty();
+    Object.keys(strawpolls[num].choices).forEach(function(c) {
+        box.append("<p>" + c + ": " + strawpolls[num].choices[c] + "</p>")
+    });
 };
 
 $(document).ready(function() {
-    $("#b3").click(function(){
-      num = (num + 1) % strawpolls.length;
-      draw();
+    $("#b3").click(function() {
+        num = (num + 1) % strawpolls.length;
+        draw();
     });
-    $("#b1").click(function(){
-     num -= 1;
-     if(num < 0){num = 0}
+    $("#b1").click(function() {
+        num -= 1;
+        if (num < 0) {
+            num = 0
+        }
 
-      draw();
+        draw();
     });
-    $("#b2").click(function(){
-      // vote(..., ...)
-      drawResults()
+    $("#b2").click(function() {
+      choice = $("input[name=choice]:checked").val();
+        // ask the DOM who is being voted for
+        // update strawpolls to add 1 vote to the right pool/choice
+        poll_id = num
+        console.log(choice)
+        console.log(poll_id)
+        $.post("/strawpolls/" + poll_id + "/vote/" + choice, {}).done(function (data) {
+          strawpolls[poll_id] = data;
+          drawResults();
+        })
     });
 
     $.getJSON('strawpolls').done(function(json) {
